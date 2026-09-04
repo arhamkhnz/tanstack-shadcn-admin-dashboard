17e593179681137841d0e730603355051206db38
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2026-09-05 00:36:01 +0530

    chore: remove new badges

--- src/navigation/sidebar/sidebar-items.ts
+++ src/navigation/sidebar/sidebar-items.ts
@@ -129,14 +129,12 @@ export const sidebarItems: NavGroup[] = [
         title: "File Manager",
         url: "/dashboard/file-manager",
         icon: FolderOpen,
-        badge: "new",
       },
       {
         id: "patient-monitoring",
         title: "Patient Monitoring",
         url: "/dashboard/patient-monitoring",
         icon: HeartPulse,
-        badge: "new",
       },
     ],
   },
@@ -185,7 +183,6 @@ export const sidebarItems: NavGroup[] = [
         title: "Profile",
         url: "/dashboard/profile",
         icon: UserRound,
-        badge: "new",
       },
       {
         id: "users",
